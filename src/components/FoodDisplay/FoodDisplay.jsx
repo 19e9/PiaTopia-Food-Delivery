6ddc58b7258f67da9b27690ab49c3601cd764901
~ Khal!d <Khaledtg17@gmail.com>
2024-04-29 17:28:49 +0300

    increase and decrease item & Css code has been added.

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -8,7 +8,7 @@ const FoodDisplay = ({category}) => {
 
   return (
     <div className='food-display' id='food-display'>
-        <h2>Size sunlnan en güzel yemekler</h2>
+        <h2>Size sunulan en güzel yemekler</h2>
         <div className="food-display-list">
             {food_list.map((item,index) =>{
                 return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
