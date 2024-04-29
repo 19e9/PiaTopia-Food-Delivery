278d0dd055a672d83f558e2b4d0a771168cabbf0
~ Khal!d <Khaledtg17@gmail.com>
2024-04-29 09:07:14 +0300

    menu list has been added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -104,7 +104,7 @@ export const menu_list = [
         menu_image: menu_5
     },
     {
-        menu_name: "Pur vge",
+        menu_name: "Ev Yemekleri",
         menu_image: menu_6
     },
     {
--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import './ExploreMenu.css'
 import { menu_list} from '../../assets/assets'
-const ExploreMenu = () => {
+
+const ExploreMenu = ({category,setCategory}) => {
+
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Menümüzü keşfedin</h1>
-        <p className='explore-menu-text'>Nefis yemek çeşitlerinin yer aldığı zengin menüden seçim yapın. Misyonumuz, iştahınızı tatmin etmek ve her seferinde lezzetli bir yemek yiyerek yemek deneyiminizi geliştirmektir.</p>
+        <p className='explore-menu-text'>Nefis yemek çeşitlerinin yer aldığı 
+        zengin menüden seçim yapın. Misyonumuz, iştahınızı tatmin etmek 
+        ve her seferinde lezzetli bir yemek yiyerek yemek deneyiminizi geliştirmektir.</p>
         <div className="explore-menu-list">
             {menu_list.map((item,index)=>{
                 return(
-                    <div key={index} className='explore-menu-list-item'>
-                        <img src={item.menu_image} alt="" />
+                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                        <img className={category===item.menu_name?"active":""} src={item.menu_image}/>
                         <p>{item.menu_name}</p>
                     </div>
                 )
             })}
         </div>
+        <hr />
     </div>
   )
 }
