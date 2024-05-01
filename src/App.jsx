34d1977ced1309e6648f87a059ccc7941c1cc075
~ Khal!d <Khaledtg17@gmail.com>
2024-05-01 23:28:53 +0300

    Footer has been added

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 import { Route, Routes } from 'react-router-dom'
 
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/order' element={<PlaceOrder/>} />
-      </Routes>
-    </div>
+    // Footer'de components'ta bir hata vermemsi için bir tane boş tag açtım bütün tagları orda aktardım.
+    <>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+
   )
 }
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+    return (
+        <div className='footer' id='footer'>
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="logo" />
+                    <p>2024 yılının Hazıran ayında hizmetinize giren Pietopia Restaurant her geçen gün sizlere daha da kaliteli hizmet vermeyi görev bilen deneyimli ekibi ile Samsun'da yeme- içme kültürü ve alışkanlıklarını değiştiren işletme politikası ile eğlence anlayışına yeni bir bakış açısı getirmek istiyor.</p>
+                    <div className="footer-soical-icons">
+                        <img src={assets.facebook_icon} alt="facebook" />
+                        <img src={assets.twitter_icon} alt="twitter" />
+                        <img src={assets.linkedin_icon} alt="linkedin" />
+                    </div>
+                </div>
+                <div className="footer-content-center">
+                    <h2>Company</h2>
+                    <ul>
+                      <li>AnaSayfa</li>
+                    <li>Hakkımızda</li>
+                    <li>Kullanım koşulları</li>
+                    <li>Gizlilik politikası</li>  
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>İletişim</h2>
+                    <ul>
+                        <li>0 850 888 90 24</li>
+                        <li>iletişim@pietopia.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr />
+            <p className="footer-copyright">Copyright © PieTopia 2024. Tüm hakları saklıdır.</p>
+        </div>
+    )
+}
+
+export default Footer
