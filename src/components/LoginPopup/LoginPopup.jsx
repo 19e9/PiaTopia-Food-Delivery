c74cf4692afc0fc4db012175aaa2300e5e12b5a7
~ Khal!d <Khaledtg17@gmail.com>
2024-05-06 23:04:38 +0300

    Sign in & Sign Up components has been completed

---
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import './LoginPopup.css'
+import { assets } from '../../assets/assets'
+
+const LoginPopup = ({ setShowLogin }) => {
+
+    const [currState, setCurrState] = useState("Girişyap")
+
+    const [phoneValue, setPhoneValue] = useState('');
+
+    return (
+        <div className='login-popup'>
+            <form className='login-popup-container'>
+                <div className="login-popup-title">
+                    <h2>{currState}</h2>
+                    <img onClick={() => setShowLogin(false)} src={assets.cross_icon} />
+                </div>
+                <div className="login-popup-inputs">
+                    {currState === "Girişyap" ? <></> : <input type='text' placeholder='Adınız' required />}
+
+                    {currState === "Girişyap" ? <></> : <input
+                        type='tel'
+                        placeholder='Telefon Numaranız'
+                        maxLength="11"
+                        onKeyPress={(e) => {
+                            if ((e.target.value.length === 0 && e.key !== '0') || (e.target.value.length > 0 && !/^[0-9]*$/.test(e.key)) || e.target.value.length >= 11) {
+                                e.preventDefault();
+                            }
+                        }}
+                        required
+                    />}
+
+                    <input type="email" placeholder='E-posta' required />
+                    <input type="password" placeholder='Şifre' required />
+                </div>
+                <button>{currState === "Üye Ol" ? "Üye Ol" : "Girişyap"}</button>
+                <div className="login-popup-condition">
+                    <input type="checkbox" required />
+                    <p>Kullanım koşullarını ve gizlilik politikasını kabul ediyorum.</p>
+                </div>
+                {currState === "Girişyap"
+                    ? <p>Üye Olmak İstiyor musunuz ? <span onClick={() => setCurrState("Üye Ol")}>Üye Ol</span></p>
+                    : <p>Zaten hesapım var ? <span onClick={() => setCurrState("Girişyap")}>Girişyap</span></p>
+                }
+            </form>
+        </div>
+    )
+}
+
+export default LoginPopup
