065c039407727f69f3b59fff0179dd1af83608b1
~ Khal!d <Khaledtg17@gmail.com>
2024-05-07 15:46:13 +0300

    Added link to home page in logo .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom'
 
 const Navbar = ({setShowLogin}) => {
 
-    const [menu,setMenu] = useState("menu")
+    const [menu,setMenu] = useState("home")
 
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
+        <Link to='/'><img src={assets.logo} alt="logo" className="logo" /></Link>
         <ul className="navbar-menu">
             <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Anasayfa</Link>
             <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menü</a>
@@ -20,7 +20,7 @@ const Navbar = ({setShowLogin}) => {
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
+                
                 <div className="dot"></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>Girişyap</button>
