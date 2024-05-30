f90c24ba9a410648e3fe15f170199d036b2633b2
~ Khal!d <Khaledtg17@gmail.com>
2024-05-30 21:51:58 +0300

    Add basic structure for cart management functions in cartController.

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,19 @@
+import userModel from "../models/userModel.js"
+
+// add items to user cart
+const addToCart = async (req,res) => {
+
+}
+
+// reomve items from user cart
+const removeFromCart = async (req,res) => {
+
+}
+
+
+// fetch user cart data
+const getCart = async (req,res) => {
+
+}
+
+export { addToCart, removeFromCart, getCart }
