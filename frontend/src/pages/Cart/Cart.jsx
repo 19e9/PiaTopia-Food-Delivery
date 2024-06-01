edab7aa4ea1898867611d262896c373281c56416
~ Khal!d <Khaledtg17@gmail.com>
2024-06-01 14:11:14 +0300

    Bug fixes.

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,15 +1,15 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import { StoreContext } from '../../context/StoreContext'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-
-  const { cartItems, food_list, removeFromCart, getTotalCartAmount, url } = useContext(StoreContext)
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount, url } =
+    useContext(StoreContext);
   const navigate = useNavigate();
 
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Ürün</p>
@@ -21,22 +21,23 @@ const Cart = () => {
         </div>
         <br />
         <br />
-        {food_list.map((item, index) => {
+        {food_list.map((item) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div>
-                <div className='cart-items-title cart-items-item'>
-                  <img src={url+"/images/"+item.image} />
+              <div key={item._id}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={`${url}/images/${item.image}`} alt={item.name} />
                   <p>{item.name}</p>
                   <p>{item.price} ₺</p>
                   <p>{cartItems[item._id]}</p>
                   <p>{item.price * cartItems[item._id]} ₺</p>
-                  <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
                 </div>
                 <hr />
               </div>
-
-            )
+            );
           }
         })}
       </div>
@@ -46,32 +47,32 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>Ürün Fiyatı </p>
-              <p>{(getTotalCartAmount()*0.82).toFixed(2)}</p>
+              <p>{(getTotalCartAmount() * 0.82).toFixed(2)}</p>
             </div>
             <div className="cart-total-details">
               <p>KDV</p>
-              <p>{(getTotalCartAmount()*0.18).toFixed(2)}</p>
+              <p>{(getTotalCartAmount() * 0.18).toFixed(2)}</p>
             </div>
             <div className="cart-total-details">
               <p>Toplam</p>
-              
-              <p>{(getTotalCartAmount()).toFixed(2)} ₺</p>
+
+              <p>{getTotalCartAmount().toFixed(2)} ₺</p>
             </div>
           </div>
-          <button onClick={() => navigate('/order')}>Sepeti Onayla</button>
+          <button onClick={() => navigate("/order")}>Sepeti Onayla</button>
         </div>
         <div className="cart-promocode">
           <div>
             <p>İndirim Kodum var</p>
-            <div className='cart-promocode-input'>
-              <input type="text" placeholder='İndirim Kodu Gir' />
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="İndirim Kodu Gir" />
               <button>Uygula</button>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
